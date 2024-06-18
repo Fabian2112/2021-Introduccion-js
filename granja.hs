715a1100768b29abbf7e047e990d7c726829a817
Fabian Montes <67841089+Fabian2112@users.noreply.github.com>
2024-06-18 14:57:58 -0300

    Actualizar granja.hs

--- granja.hs
+++ granja.hs
@@ -5,11 +5,14 @@ data animal = animal {
    edad :: Number
    estaEnfermo :: Bool
    diasDeRecuperacion :: Number
+   costo :: Number
 } Deriving (Show, Eq)
 
 data veterinario = Veterinario{
     diagnostico :: visitaMedica
+    diasDeRecuperacion :: Number
     costo :: Number
+    vitaminas :: Number
 } Deriving (Show, Eq)
     
 
@@ -22,4 +25,46 @@ laPasoMal animal = (>30) $ diasDeRecuperacion animal
 
 
 nombreFalopa animal = (=='i') . last $ nombre animal
+
+
+engorde :: kilosComida -> animal -> animal
+
+engorde kilosComida animal = animal {
+   peso = (+) max (5, kilosComida/2) $ peso animal
+}
+
+
+visitaMedica veterinario animal = animal {
+   estaEnfermo = False
+   diasDeRecuperacion = diasDeRecuperacion veterinario
+   costo = costo veterinario
+}
+   
+
+
+revisacion :: veterinario -> animal -> animal
+
+revisacion veterinario animal = (+2) peso . (diagnostico veterinario) $ animal
+
+
+festejoCumple :: animal -> animal
+
+festejoCumple animal = animal {
+ edad = (+1) edad animal
+ peso = (-1) peso animal
+}
+
+
+chequeoDePeso :: pesoLimite -> animal -> animal
+
+chequeoDePeso pesoLimite animal
+   | peso animal < pesoLimite = animal{estaEnfermo=True}
+   | otherwise = animal{estaEnfermo=False}
+
+
+type proceso = [actividad]
+
+proceso animal  = foldl ($) animal proceso
+
+
 
